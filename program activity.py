2a0701cb334d70e89201e61323853d99378733e2
SirFavour <favour.nedu@gmail.com>
2023-05-31 01:08:06 -0400

    Program Activity
    Basic Arithmetic calculator program

---
+++ program activity.py
@@ -0,0 +1,34 @@
+#Solution:
+# Basic Python Program to perform Arithmetic functions
+
+# Definition of the add function
+def add(a,b):
+  return a+b
+
+# Definition of the subtract function
+def subtract(a,b):
+  return a-b
+
+# Definition of the multiply function
+def multiply(a,b):
+  return a*b
+
+# Definition of the divide function
+def divide(a,b):
+  return a/b
+
+# Input values
+a = 10
+b = 5
+
+# Call the add function
+print("The sum of",a,"and",b,"is", add(a,b))
+
+# Call the subtract function
+print("The difference of",a,"and",b,"is", subtract(a,b))
+
+# Call the multiply function
+print("The product of",a,"and",b,"is", multiply(a,b))
+
+# Call the divide function
+print("The division of",a,"and",b,"is", divide(a,b))
